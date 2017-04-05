f21653688be2218e8e0e4e11b4c841e806450aa3
Mateusz Kaczmarek <mateusz@kacz.pl>
2017-04-05 17:21:47 +0200

    Working app

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,66 @@ var Gimmie = {
   toggleLoading: function() {
     this.content.classList.toggle('content--loading');
     var button = this.form.querySelector('button');
-    button.setAttribute('disabled', true);
   },
   userInput: '',
   userInputIsValid: false,
   appId: '',
   urlToAsk: '',
+  throwError: function(header, text) {
+    var errorMsg = '<p><strong>' + header + '</strong>' + text + '</p>';
+    this.content.innerHTML = errorMsg;
+    this.content.classList.add('content--error');
+    this.content.classList.add('content--error-pop');
+    this.toggleLoading();
+    setTimeout(function() {
+      Gimmie.content.classList.remove('content--error-pop');
+    }, 333);
+  },
+  doRequest: function(url) {
+    var head = document.head;
+    var script = document.createElement("script");
+
+    script.setAttribute("src", url);
+    head.appendChild(script);
+    head.removeChild(script);
+  },
+  requestCallback: function (data) {
+    var response = data.results[0];
+    if(response && response.artworkUrl512 != null){
+      Gimmie.render(response);
+    } else {
+      Gimmie.throwError(
+        'Invalid Response',
+        'The request you made appears to not have an associated icon. <br> Try a different URL.'
+      );
+    }
+  },
+  render: function(response) {
+    var result = document.createDocumentFragment();
+
+    var icon = new Image();
+    icon.src = response.artworkUrl512;
+    result.appendChild(icon);
+    var h4 = document.createElement('h4');
+    h4.innerHTML = response.trackName;
+    result.appendChild(h4);
+
+    while (Gimmie.content.hasChildNodes()) {
+      Gimmie.content.removeChild(Gimmie.content.lastChild);
+    }
+    Gimmie.content.classList.remove('content--error');
+    Gimmie.content.appendChild(result);
+
+    Gimmie.toggleLoading();
+
+    if(response.kind != 'mac-software') {
+      var mask = new Image();
+      mask.src = 'assets/img/icon-mask.png';
+      mask.onload = function() {
+          Gimmie.content.prepend(this);
+      }
+    }
+  },
   validate: function(input) {
     var regUrl = /^(http|https):\/\/itunes/;
     var regId = /\/id(\d+)/i;
@@ -18,19 +72,12 @@ var Gimmie = {
       this.userInputIsValid = true;
       var id = regId.exec(this.userInput);
       this.appId = id[1];
-      this.urlToAsk = 'https://itunes.apple.com/lookup?id=' + this.appId;
+      this.urlToAsk = 'https://itunes.apple.com/lookup?id=' + this.appId + '&callback=Gimmie.requestCallback';
     } else {
       this.userInputIsValid = false;
       this.appId = '';
       this.urlToAsk = '';
     }
-  },
-  throwError: function(header, text) {
-    var errorMsg = '<p><strong>' + header + '</strong>' + text + '</p>';
-    this.content.innerHTML = errorMsg;
-    this.content.classList.toggle('content--error');
-    this.content.classList.toggle('content--error-pop');
-    this.toggleLoading();
   }
 }
 
@@ -42,20 +89,7 @@ document.addEventListener("DOMContentLoaded", function() {
     Gimmie.validate();
 
     if(Gimmie.userInputIsValid) {
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.responseText);
-        }
-      };
-
-      xhttp.open("GET", Gimmie.urlToAsk, true);
-      xhttp.send();
-
-      xhttp.addEventListener("load", function(data) {
-        alert('sukces');
-        console.log(data);
-      });
+      Gimmie.doRequest(Gimmie.urlToAsk);
     } else {
       Gimmie.throwError(
         'Invalid link',
